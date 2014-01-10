056023db7f1f50763616386edae5ad5d5f1de220
Vavius <vavius@gmail.com>
2014-01-11 01:52:23 +0400

    new window creation func

--- src/display/App.lua
+++ src/display/App.lua
@@ -12,9 +12,8 @@ local DEFAULT_WINDOW = {
     viewWidth = 320,
     viewHeight = 480,
     scale = "letterbox",
+    viewOffset = {0, 0},
 }
-
-local DEFAULT_VIEW_OFFSET = {0, 0}
 
 ---
 -- Create moai window
@@ -24,40 +23,56 @@ function App:openWindow(title, windowParams)
     windowParams = windowParams or DEFAULT_WINDOW
     title = title or "MOAI"
 
-    screenWidth     = windowParams.screenWidth or DEFAULT_WINDOW.screenWidth
-    screenHeight    = windowParams.screenHeight or DEFAULT_WINDOW.screenHeight
-    viewWidth       = windowParams.viewWidth or DEFAULT_WINDOW.viewWidth
-    viewHeight      = windowParams.viewHeight or DEFAULT_WINDOW.viewHeight
-    scaleMode       = windowParams.scale or DEFAULT_WINDOW.scale
+    for k, v in pairs(DEFAULT_WINDOW) do
+        if not windowParams[k] then
+            windowParams[k] = v
+        end
+    end
 
     Runtime:initialize()
     RenderMgr:initialize()
     InputMgr:initialize()
     SceneMgr:initialize()
 
+    Runtime:addEventListener("resize", self.onResize, self)
+
+    self.screenWidth = windowParams.screenWidth
+    self.screenHeight = windowParams.screenHeight
+    
+    self:updateVieport(params)
+
     MOAISim.openWindow(title, screenWidth, screenHeight)
+end
 
-    self.screenWidth = screenWidth
-    self.screenHeight = screenHeight
+--- 
+--
+--
+function App:onResize(event)
+    self.screenWidth = event.width
+    self.screenHeight = event.height
 
-    self:updateVieport(viewWidth, viewHeight, scaleMode, DEFAULT_VIEW_OFFSET)
+    self:updateVieport(self.windowParams)
 end
 
 ---
 -- 
 -- 
-function App:updateVieport(width, height, scaleMode, offset)
+function App:updateVieport(params)
+    local width = params.viewWidth
+    local height = params.viewHeight
+
     local wRatio = self.screenWidth / width
     local hRatio = self.screenHeight / height
-    if scaleMode == "letterbox" then
+    if params.scaleMode == "letterbox" then
         self.viewWidth = (wRatio > hRatio) and width * wRatio / hRatio or width
         self.viewHeight = (hRatio > wRatio) and height * hRatio / wRatio or height
     end
 
+    self.windowParams = params
     self.viewport = self.viewport or MOAIViewport.new()
     self.viewport:setSize(self.screenWidth, self.screenHeight)
     self.viewport:setScale(self.viewWidth, self.viewHeight)
-    self.viewport:setOffset(offset[1], offset[2])
+    self.viewport:setOffset(params.viewOffset[1], params.viewOffset[2])
 end
 
 ---
