a0d82fee06a2461fa812370308a7d6160c0be176
Vasiliy Yanushevich <vavius@gmail.com>
2014-04-07 18:35:00 +0400

    coroutine extensions

--- src/util/util.lua
+++ src/util/util.lua
@@ -154,6 +154,24 @@ function math.lerp(a, b, t)
 end
 
 --------------------------------------------------------------------------------
+-- Coroutine extensions
+--------------------------------------------------------------------------------
+-- skip n steps
+-- step duration is defined by MOAISim settings (simulation step)
+function coroutine.skip(steps)
+    for i = 1, steps do
+        coroutine.yield()
+    end
+end
+
+-- wait given number of seconds
+function coroutine.wait(time)
+    while time > 0 do
+        time = time - coroutine.yield()
+    end
+end
+
+--------------------------------------------------------------------------------
 -- String extensions
 --------------------------------------------------------------------------------
 local DEFAULT_SEP = '/'
